52093feb9aac208d72edfe290f15c3110bf9c615
Rubem Fridolino Christ Neto <56802722+RubemNto@users.noreply.github.com>
2022-01-04 20:00:56 +0000

    remake design and register

--- FinalProject/app/src/main/java/ipca/project/rpglife/LoginPage.kt
+++ FinalProject/app/src/main/java/ipca/project/rpglife/LoginPage.kt
@@ -14,98 +14,25 @@ import com.google.firebase.ktx.Firebase
 
 class LoginPage : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
-    private lateinit var SignInButton: Button
-    private lateinit var RegisterButton: Button
+    lateinit var signInButton: Button;
+    lateinit var registerButton: Button;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        auth = Firebase.auth
         setContentView(R.layout.activity_login_page)
-        SignInButton = findViewById(R.id.SignInButton)
-        RegisterButton = findViewById(R.id.RegisterButton)
-        SignInButton.setOnClickListener {
-            signIn(
-                findViewById<TextView>(R.id.UsernameTextView).text.toString(),
-                findViewById<TextView>(R.id.PasswordTextView).text.toString()
-            )
+
+        signInButton = findViewById(R.id.SignInButton)
+        registerButton = findViewById(R.id.RegisterButton)
+
+        signInButton.setOnClickListener {
+            val intent = Intent(this,MapActivity::class.java)
+            startActivity(intent)
         }
 
-        RegisterButton.setOnClickListener {
-            register(
-                findViewById<TextView>(R.id.UsernameTextView).text.toString(),
-                findViewById<TextView>(R.id.PasswordTextView).text.toString()
-            )
+        registerButton.setOnClickListener {
+            val intent = Intent(this,Register::class.java)
+            startActivity(intent)
         }
     }
 
-    public override fun onStart() {
-        super.onStart()
-        val currentUser = auth.currentUser
-        if (currentUser != null) {
-            reload()
-        }
-    }
-
-    private fun startMainActivity()
-    {
-        val intent = Intent(this@LoginPage,MainActivity::class.java)
-        startActivity(intent)
-    }
-
-    private fun register(email: String, password: String) {
-        // [START create_user_with_email]
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "createUserWithEmail:success")
-                    val user = auth.currentUser
-                    updateUI(user)
-                    startMainActivity()
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    updateUI(null)
-                }
-            }
-        // [END create_user_with_email]
-    }
-
-    private fun signIn(email: String, password: String) {
-        // [START sign_in_with_email]
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithEmail:success")
-                    val user = auth.currentUser
-                    updateUI(user)
-                    startMainActivity()
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    updateUI(null)
-                }
-            }
-        // [END sign_in_with_email]
-    }
-
-    private fun updateUI(user: FirebaseUser?) {
-    }
-
-    private fun reload() {
-    }
-
-    companion object {
-        private const val TAG = "EmailPassword"
-    }
 }
